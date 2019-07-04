2bda15d042d15b0a338f0a4a3eb5917e2943030d
Sam Bahta <smabahta37@gmail.com>
2019-07-03 22:03:32 -0400

    finished rework

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import Animated from 'react-native-reanimated';
 
@@ -7,46 +7,79 @@ const {
   set,
   cond,
   eq,
-  add,
   spring,
   startClock,
   stopClock,
-  cloclRunning,
-  sub,
+  clockRunning,
   defined,
   Value,
   Clock,
   event,
 } = Animated;
 
+function runSpring(clock, value, velocity, dest) {
+  const state = {
+    finished: new Value(0),
+    velocity: new Value(0),
+    position: new Value(0),
+    time: new Value(0)
+  };
+
+  const config = {
+    damping: 7,
+    mass: 1,
+    stiffness: 121.6,
+    overshootClamping: false,
+    restSpeedThreshold: 0.001,
+    restDisplacementThreshold: 0.001,
+    toValue: new Value(0)
+  };
+
+  return [
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.velocity, velocity),
+      set(state.position, value),
+      set(config.toValue, dest),
+      startClock(clock)
+    ]),
+    spring(clock, state, config),
+    cond(state.finished, stopClock(clock)),
+    state.position
+  ];
+}
+
 export default class App extends React.Component {
   constructor() {
     super();
-    this.translateX = new Value(0)
-    const dragX = new Value(0)
-    const state = new Value(-1)
-
+    this.translateX = new Value(0);
+    const dragX = new Value(0);
+    const state = new Value(-1);
+    const dragVX = new Value(0)
     this.onGestureEvent = event([
       {
         nativeEvent: {
           translationX: dragX,
+          velocityX: dragVX,
           state: state
         }
       }
     ]);
-
+    const clock = new Clock()
     const transX = new Value();
     this.translateX = cond(
       eq(state, State.ACTIVE),
       [
         //state active
-        set(transX,dragX),
+        stopClock(clock),
+        set(transX, dragX),
         transX
       ],
       [
         //state inactive
-        set(transX,0),
-        transX
+        set(transX, cond(
+          defined(transX), runSpring(clock, transX, dragVX, 0), 0
+        )),
       ])
   }
 
